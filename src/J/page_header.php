795b38c7822714676b1d4f86a9a2e759bb7adc93
ydzhony1 <ydzhony1@163.com>
2011-10-12 17:08:13 +0800

    fix old frame ,modify php

---
+++ src/J/page_header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>jackyang Demo --frame dialog</title>
+<link rel="stylesheet" href="../../css/frame_ui.css" />
+<link rel="stylesheet" href="../../css/page_ui.css" />
+<script src="../../js/jy.js"></script>
+</head>
+<style>
+.lnkshow{
+	background:#F00;
+	color:#fff;
+	display:inline-block;
+	padding:5px;
+}
+.text{
+	display:none;
+}
+</style>
+<body>
